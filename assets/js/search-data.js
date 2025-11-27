d616eecc12b188c641b5c113a1937ccdcdc8a7a2
DiegoFE94 <diegolas94@hotmail.com>
2025-11-27 16:43:08 +0000

    Deploying to gh-pages from @ MALL-Lab/MALL-Lab.github.io@4f9274ef41c521f5505ec645cae535e4280fda90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,42 +41,7 @@ ninja.data = [{
             },},{id: "news-adding-info-about-us-publications-projects-and-teams",
           title: 'Adding info about us, publications, projects and teams!',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-precision",
+          section: "News",},{id: "projects-precision",
           title: 'PRECISION',
           description: "Explainable graph convolutional networks for innovation in precision oncology and drug resistance in cancer",
           section: "Projects",handler: () => {

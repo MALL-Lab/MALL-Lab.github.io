67784dc5ae218745b8a94acb430b201fbd0cbf53
DiegoFE94 <diegolas94@hotmail.com>
2025-11-27 15:22:08 +0000

    Deploying to gh-pages from @ MALL-Lab/MALL-Lab.github.io@19af4d763678cccf0a70e472014f23c498f0c46a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,12 +41,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -86,6 +81,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-repo-sudoe-project",
+          title: 'RePo-SUDOE project',
+          description: "Drug Repurposing for Effective and Accelerated Drug Development in the SUDOE Space",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/REPOSUDOE_project/";
             },},{
         id: 'social-email',
         title: 'email',

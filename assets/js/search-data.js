8bc34ed7633677c2be51d28fb34bc07810230cf1
DiegoFE94 <diegolas94@hotmail.com>
2025-11-27 16:06:29 +0000

    Deploying to gh-pages from @ MALL-Lab/MALL-Lab.github.io@a6cf7c879bad51dc68cc5873f9e15ff6735f2a5b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,12 +41,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
@@ -81,6 +76,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-precision",
+          title: 'PRECISION',
+          description: "Explainable graph convolutional networks for innovation in precision oncology and drug resistance in cancer",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/PRECISION_project/";
             },},{id: "projects-repo-sudoe-project",
           title: 'RePo-SUDOE project',
           description: "Drug Repurposing for Effective and Accelerated Drug Development in the SUDOE Space",
